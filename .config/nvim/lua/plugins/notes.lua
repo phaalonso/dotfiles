ed736807d54ac9abea2d043f1867c30e45c3bc74
Pedro Alonso <phaalonso@gmail.com>
2023-07-24 11:40:34 -0300

    Incluindo plugin do obsidian

---
+++ .config/nvim/lua/plugins/notes.lua
@@ -0,0 +1,46 @@
+return {
+  "epwalsh/obsidian.nvim",
+  lazy = true,
+  event = { "BufReadPre /home/pedro/Documents/Notes/**.md" },
+  -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand':
+  -- event = { "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md" },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- Optional, for completion.
+    "hrsh7th/nvim-cmp",
+
+    -- Optional, for search and quick-switch functionality.
+    "nvim-telescope/telescope.nvim",
+
+    -- Optional, an alternative to telescope for search and quick-switch functionality.
+    -- "ibhagwan/fzf-lua"
+
+    -- Optional, another alternative to telescope for search and quick-switch functionality.
+    -- "junegunn/fzf",
+    -- "junegunn/fzf.vim"
+
+    -- Optional, alternative to nvim-treesitter for syntax highlighting.
+    "godlygeek/tabular",
+    "preservim/vim-markdown",
+  },
+  opts = {
+    dir = "/home/pedro/Documents/Notes/", -- no need to call 'vim.fn.expand' here
+
+    -- see below for full list of options 👇
+  },
+  config = function(_, opts)
+    require("obsidian").setup(opts)
+
+    -- Optional, override the 'gf' keymap to utilize Obsidian's search functionality.
+    -- see also: 'follow_url_func' config option below.
+    vim.keymap.set("n", "gf", function()
+      if require("obsidian").util.cursor_on_markdown_link() then
+        return "<cmd>ObsidianFollowLink<CR>"
+      else
+        return "gf"
+      end
+    end, { noremap = false, expr = true })
+  end,
+}
